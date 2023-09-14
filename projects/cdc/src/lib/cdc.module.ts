7ac40c4db04373d79f8e37be46fdf2e0c33f65a0
Renaud Dubuis <renaud.dubuis@decryptage.net>
2023-09-14 14:55:44 +0200

    Preparation Library

--- projects/cdc/src/lib/cdc.module.ts
+++ projects/cdc/src/lib/cdc.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CdcComponent } from './cdc.component';
-
-
 
 @NgModule({
   declarations: [
-    CdcComponent
   ],
   imports: [
   ],
   exports: [
-    CdcComponent
   ]
 })
 export class CdcModule { }
--- projects/cdc/src/public-api.ts
+++ projects/cdc/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of cdc
  */
 
-export * from './lib/cdc.service';
-export * from './lib/cdc.component';
 export * from './lib/cdc.module';
