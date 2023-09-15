bc087e14bdcf558f06b91b0a51730debad2ec241
Renaud Dubuis <renaud.dubuis@decryptage.net>
2023-09-15 12:36:43 +0200

    ReHttpClientfactoring

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { PresenterDirective } from './directives/presenter.directive';
 import { LoggerMiddlewareService } from './middlewares/presenter/logger.middleware.service';
 import { ProductMiddlewareService } from './middlewares/presenter/product.middleware.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     PresenterDirective
   ],
   imports: [
-    CommonModule
+    HttpClientModule
   ],
   exports: [
     PresenterDirective
--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+
+  constructor(
+    @Inject(HttpClient) private http:HttpClient
+  ){}
 
   getProducts(){
     return fetch('https://dummyjson.com/products')
