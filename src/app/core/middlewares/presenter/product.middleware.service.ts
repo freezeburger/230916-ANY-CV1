e603f0ac2b988e7c7f30e444c6d07e649291e6da
Renaud Dubuis <renaud.dubuis@decryptage.net>
2023-09-15 12:47:38 +0200

    Refactoring

--- src/app/core/middlewares/presenter/product.middleware.service.ts
+++ src/app/core/middlewares/presenter/product.middleware.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { PresenterService } from '../../services/presenter.service';
 import { ProductService } from '../../services/product.service';
 import { ActionEnum } from '../../interfaces/presenter/action.enum';
+import { filter, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +14,14 @@ export class ProductMiddlewareService {
     productSrv:ProductService
   ) {
 
-    presenterSrv.action$.subscribe( async action => {
-
-      if( action?.type !== ActionEnum.PRODUCT_LIST_REQUEST) return;
-
-      const products = await productSrv.getProducts();
-      presenterSrv.dispatch({type:ActionEnum.MESSAGE_LIST_UPDATE, payload:products})
-
-    })
+    presenterSrv.action$
+      .pipe(
+        filter( action => action?.type === ActionEnum.PRODUCT_LIST_REQUEST ),
+        switchMap( action => productSrv.getProducts() )
+      )
+      .subscribe( products => {
+        presenterSrv.dispatch({type:ActionEnum.MESSAGE_LIST_UPDATE, payload:products})
+      })
 
    }
 }
