59c0d0e44f51df47739279244547ee14118add71
Renaud Dubuis <renaud.dubuis@decryptage.net>
2023-09-15 14:25:08 +0200

    Routing

--- src/app/domains/message/message.module.ts
+++ src/app/domains/message/message.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MessageListComponent } from './message-list/message-list.component';
 
-
-
 @NgModule({
   declarations: [
     MessageListComponent
