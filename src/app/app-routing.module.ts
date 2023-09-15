b8afdacedc47b2aae0e85c348410347654e02d1d
Renaud Dubuis <renaud.dubuis@decryptage.net>
2023-09-15 15:12:42 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'auth'
+  },
+  {
+    path:'products'
+  },
+  {
+    path:'messages'
+  },
+  {
+    path:'**',
+    redirectTo:'auth',
+    pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
