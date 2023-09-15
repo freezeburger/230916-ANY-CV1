038a609d2667d273d1eae7428dadfe8570d3db27
Renaud Dubuis <renaud.dubuis@decryptage.net>
2023-09-15 15:05:32 +0200

    Routing

--- src/app/domains/auth/auth-routing.module.ts
+++ src/app/domains/auth/auth-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewLoginComponent } from './views/view-login/view-login.component';
+import { ViewRegisterComponent } from './views/view-register/view-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component:ViewLoginComponent
+  },
+  {
+    path:'register',
+    component:ViewRegisterComponent
+  },
+  {
+    path:'**',
+    redirectTo:'register',
+    pathMatch:'prefix'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
