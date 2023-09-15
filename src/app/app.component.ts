a097fc6bfb7fa3189669b4b4248856cec3328a07
Renaud Dubuis <renaud.dubuis@decryptage.net>
2023-09-15 16:56:03 +0200

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { PresenterDirective } from './core/directives/presenter.directive';
+import { ActionEnum } from './core/interfaces/presenter/action.enum';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = '230916-ANY-CV1';
+export class AppComponent extends PresenterDirective {
+
+  clear(){
+    this.presenter.dispatch({type:ActionEnum.APP_NOTIFICATION, payload:null})
+  }
 }
